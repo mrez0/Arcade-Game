281c0d9102d3bd3d037c23d620602d79c9c0bf50
Unknown <ahmedmohsen1337@hotmail.com>
2018-06-11 16:22:05 +0300

    Implement settings options

--- js/app2.js
+++ js/app2.js
@@ -78,12 +78,38 @@ const gameBoundaries = {
 
 class Game {
     constructor() {
+        this.createNewSprites();
+
         this.score = 0;
 
         this.updateScore();
 
         //Flag when game is stopped
         this.stopped = false;
+    }
+
+    createNewSprites() {
+        //Creating enemies
+        allEnemies = [];
+        const numEnemies = difficulty[settings.difficulty].enemy.numEnemies;
+
+        for (let index = 0; index < numEnemies; index++) {
+            allEnemies.push( new Enemy() );
+        }
+
+        //Creating key
+        key = new Key();
+
+        //Creating rocks
+        rocks = [];
+        const numRocks = difficulty[settings.difficulty].numObstacles;
+
+        for (let index = 0; index < numRocks; index++) {
+            rocks.push( new Rock() );
+        }
+
+        //Creating player
+        player = new Player();
     }
 
     playerLose() {
@@ -123,14 +149,17 @@ class Game {
     }
 
     restartGame() {
-        document.getElementById('message').remove();
+        const message = document.getElementById('message');
+        if( message ) {
+            message.remove();
+        }
+
         this.stopped = false;
         player.reset();
         allEnemies.forEach( enemy => enemy.reset() );
         key.reset();
         rocks.forEach( rock => rock.reset() );
     }
-
 }
 
 class Enemy {
@@ -142,6 +171,10 @@ class Enemy {
         for (let i = 0; i < gameBoundaries.numberEnemyLanes; i++) {
             this.allowedY.push( gameBoundaries.topBoundary + gameBoundaries.rowHeight * i );
         }
+
+        //If resource loaded, width & height will be set, if not, will be set in onReady function
+        this.width = Resources.get( this.sprite ).width || 0;
+        this.height = Resources.get( this.sprite ).height || 0;
 
         //Setting width & height of enemy from sprite image once loaded
         Resources.onReady( () => {
@@ -207,7 +240,7 @@ class Enemy {
         //Setting maximum and minimum speed using setting & difficulty objects
         const max = difficulty[settings.difficulty].enemy.enemySpeed.max;
         const min = difficulty[settings.difficulty].enemy.enemySpeed.min;
-
+console.log(max);
         return Math.floor(Math.random() * ( max - min + 1)) + min;
     }
 
@@ -219,6 +252,10 @@ class Enemy {
 class Player {
     constructor() {
         this.sprite = settings.sprites.player;
+
+        //If resource loaded, width & height will be set, if not, will be set in onReady function
+        this.width = Resources.get( this.sprite ).width || 0;
+        this.height = Resources.get( this.sprite ).height || 0;
 
         //Setting width & height of player from sprite image once loaded
         Resources.onReady( () => {
@@ -302,7 +339,8 @@ class Player {
                 y = gameBoundaries.topBoundary;
             }
         }
-
+        console.log(x,y);
+        console.log(this.width);
         //Check colliding with a rock
         let isHitRock = false;
         rocks.forEach( rock => {
@@ -331,6 +369,10 @@ class Player {
 class Key {
     constructor() {
         this.sprite = settings.sprites.key;
+
+        //If resource loaded, width & height will be set, if not, will be set in onReady function
+        this.width = Resources.get( this.sprite ).width || 0;
+        this.height = Resources.get( this.sprite ).height || 0;
 
         //Setting width & height of player from sprite image once loaded
         Resources.onReady( () => {
@@ -372,6 +414,10 @@ class Rock {
     constructor() {
         this.sprite = settings.sprites.obstacle;
 
+        //If resource loaded, width & height will be set, if not, will be set in onReady function
+        this.width = Resources.get( this.sprite ).width || 0;
+        this.height = Resources.get( this.sprite ).height || 0;
+
         //Setting width & height of player from sprite image once loaded
         Resources.onReady( () => {
             this.width = Resources.get( this.sprite ).width;
@@ -410,29 +456,20 @@ class Rock {
     }
 }
 
-//Creating new game
-const game = new Game();
-
 //Creating enemies
-const allEnemies = [];
-const numEnemies = difficulty[settings.difficulty].enemy.numEnemies;
-
-for (let index = 0; index < numEnemies; index++) {
-    allEnemies.push( new Enemy() );
-}
+let allEnemies;
 
 //Creating key
-const key = new Key();
+let key;
 
-const rocks = [];
-const numRocks = difficulty[settings.difficulty].numObstacles;
-
-for (let index = 0; index < numRocks; index++) {
-    rocks.push( new Rock() );
-}
+//Creating rocks / obstacles
+let rocks;
 
 //Creating player
-const player = new Player();
+let player;
+
+//Creating new game
+const game = new Game();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -445,4 +482,62 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+});
+
+//Listen for character setting buttons
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+const currentCharacterImg = document.getElementById('character-img');
+
+const charactersImages = [
+    'images/char-boy.png',
+    'images/char-cat-girl.png',
+    'images/char-horn-girl.png',
+    'images/char-pink-girl.png',
+    'images/char-princess-girl.png'
+];
+
+prevBtn.addEventListener('click', event => {
+    const currentImgAlt = currentCharacterImg.alt;
+    let nextIndex = 0;
+
+    charactersImages.find( (currentValue, index, arr) => {
+        if( currentValue.indexOf( currentImgAlt ) > -1 ) {
+            nextIndex = index - 1;
+        }
+    });
+
+    nextIndex = nextIndex < 0 ? charactersImages.length - 1 : nextIndex;
+    const regex = /images\/(.*)\.png/;
+    const alt = charactersImages[nextIndex].match(regex);
+
+    currentCharacterImg.alt = alt[1];
+    currentCharacterImg.src = charactersImages[nextIndex];
+});
+
+nextBtn.addEventListener('click', event => {
+    const currentImgAlt = currentCharacterImg.alt;
+    let nextIndex = 0;
+
+    charactersImages.find( (currentValue, index, arr) => {
+        if( currentValue.indexOf( currentImgAlt ) > -1 ) {
+            nextIndex = index + 1;
+        }
+    });
+
+    nextIndex = nextIndex >= charactersImages.length ? 0 : nextIndex;
+    const regex = /images\/(.*)\.png/;
+    const alt = charactersImages[nextIndex].match(regex);
+
+    currentCharacterImg.alt = alt[1];
+    currentCharacterImg.src = charactersImages[nextIndex];
+});
+
+const playBtn = document.getElementById('play-btn');
+
+playBtn.addEventListener('click', event => {
+    settings.sprites.player = currentCharacterImg.getAttribute('src'); //Using getAttribute instead of src because src return the full path to th image which will not work properly when open offline.
+    settings.difficulty = document.querySelector('input[name=difficulty]:checked').value;
+    game.createNewSprites();
+    game.restartGame();
 });
--- js/engine.js
+++ js/engine.js
@@ -188,7 +188,12 @@ var Engine = (function(global) {
         'images/enemy-bug.png',
         'images/char-boy.png',
         'images/Key.png',
-        'images/Rock.png'
+        'images/Rock.png',
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
